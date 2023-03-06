c44ead7561dc6c965a72f9083e3faefb02da9607
oibasqada <tumarbekovmax@gmail.com>
2023-03-06 23:19:20 +0600

    JS first lesson hw. Slightly understood

---
+++ script.js
@@ -0,0 +1,24 @@
+// let a = 4
+// let b = 55
+// let a = 'hello'
+// let b = ' Max!'
+// console.log (a + b)
+// console.log (a * b)
+// console.log (a / b)
+// console.log (a % b)
+
+
+// let a = 55
+// let b = ' Max!'
+
+// console.log (a * b)
+
+
+// let b = '5'
+// let a = 5
+//
+// console.log (a + Number(b))
+
+// let b
+// console.log (b)
+
